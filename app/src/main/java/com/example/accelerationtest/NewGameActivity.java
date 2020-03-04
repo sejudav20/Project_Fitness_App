5a8a19150179ee84b386d38aeb7669c2bbc61972
Davin <davsej100@gmail.com>
2020-03-04 15:43:00 -0800

    New Alert Dialog finish creation

--- app/src/main/java/com/example/accelerationtest/NewGameActivity.java
+++ app/src/main/java/com/example/accelerationtest/NewGameActivity.java
@@ -1,15 +1,21 @@
 package com.example.accelerationtest;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class NewGameActivity extends AppCompatActivity {
@@ -17,15 +23,29 @@ RecyclerView rv;
 Button createGame;
 NewGamesAdapter nga;
 LiveData<List<GameData>> liveData;
+String user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
         rv=findViewById(R.id.recyclerViewGames);
         createGame=findViewById(R.id.createGameNew);
+        user= getSharedPreferences("userData",MODE_PRIVATE).getString("Username","guest");
         createGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AlertDialog.Builder adb= new AlertDialog.Builder(NewGameActivity.this);
+                adb.setTitle("New Game");
+                adb.setNegativeButton("Cancel",null);
+                View aView=getLayoutInflater().inflate(R.layout.create_new_game_alert_dialog,null);
+                final EditText edt=aView.findViewById(R.id.editNewName);
+                adb.setView(aView);
+                adb.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        GameData gd= new GameData(Calendar.getInstance().getTime(),user,edt.getText().toString(),);
+                    }
+                });
 
             }
         });
